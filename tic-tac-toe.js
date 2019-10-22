29b58dd7bbdcd5e8442bff86de960f580655c55b
Kayvia Harriott <kayviaharriott@hotmail.com>
2019-10-21 21:46:56 -0500

    Option to choose X or O added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,7 +4,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     //Variables
     let boxes = document.getElementById("board").children;
-    var counter;
+    //var Array = ['0','1','2','3','4','5','6','7','8'];
+    var Array = [];
+    var XorY = 1; //let 1 be X and 0 be O
 
     
 
@@ -14,13 +16,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
     //Actions
     
     //for loop to create 9 boxes for game
-    for (counter = 0; counter <= 8; counter++) {
+    for (let counter = 0; counter <= 8 ; counter++) {
         boxes[counter].classList.add("square");
+        boxes[counter].addEventListener("click", function(){ 
+            if (boxes[counter].innerHTML == ""){    
+                if ( XorY == 1 ){
+                    boxes[counter].innerHTML = "X" ;
+                    XorY = 0; //to change clicker to O
+                    Array[counter] = "X";
+
+                   // alert (Array);
+
+                }
+                else{
+                    boxes[counter].innerHTML = "O";
+                    XorY = 1; //to change clicker to 1
+                    Array[counter] = "O";
+                  //  alert (Array)
+
+                }
+            }
+
+        });
+
     } 
 
 
 
 
     //Functions
+    
 
 });
